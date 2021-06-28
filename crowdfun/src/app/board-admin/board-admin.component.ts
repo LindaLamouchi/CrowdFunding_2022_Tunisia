8f7ec41015d8d735576af65c4242cb39be3c84c6
linda lamouchi <lamouchilinda@gmail.com>
2021-06-28 15:19:54 +0200

    v1.0

---
+++ crowdfun/src/app/board-admin/board-admin.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_services/user.service';
+
+@Component({
+  selector: 'app-board-admin',
+  templateUrl: './board-admin.component.html',
+  styleUrls: ['./board-admin.component.css']
+})
+export class BoardAdminComponent implements OnInit {
+  content = '';
+  pageSizee:number=5;//nbr de coach a afficher sur  admin board
+  sizeCo:number; //taille de la liste  this.sizeCo=this.dataCoach.length;
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.getAdminBoard().subscribe(
+      data => {
+        this.content = data;
+      },
+      err => {
+        this.content = JSON.parse(err.error).message;
+      }
+    );
+  }
+}
+
